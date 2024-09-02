d0887c8f9076ff28d525d98ad7fb7f5029f13266
innocentkishore <smartkishorekumar57@gmail.com>
2024-09-02 15:09:56 +0530

    feat:Fc

--- src/Exercise1.java
+++ src/Exercise1.java
@@ -1,4 +1,27 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Exercise1 {
-}
+    public static void main(String[] args) {
+
+                Scanner sc = new Scanner(System.in);
+                int num1=sc.nextInt();
+                // int num2=sc.nextInt();
+                if(num1>90){
+                    System.out.println("Grade A");
+                }
+                else if(num1>80&&num1<89){
+                    System.out.println("Grade B");
+                }
+                else if(num1>70&&num1<79){
+                    System.out.println("Grade C");
+                }
+                else if(num1>60&&num1<69){
+                    System.out.println("Grade D");
+                }
+                else if(num1<60){
+                    System.out.println("Grade E");
+                }
+            }
+        }
+
+
